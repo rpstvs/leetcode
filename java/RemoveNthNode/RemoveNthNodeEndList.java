cd54b2ce4988306287d48503f7a649c4e9f3edf9
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-05-17 14:46:35 +0100

    added new solution

---
+++ java/RemoveNthNode/RemoveNthNodeEndList.java
@@ -0,0 +1,42 @@
+package java.RemoveNthNode;
+
+public class RemoveNthNodeEndList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy;
+
+        ListNode right = head;
+
+        while (n > 0) {
+            right = right.next;
+            n--;
+        }
+
+        while (right != null) {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+
+        return dummy.next;
+    }
+}

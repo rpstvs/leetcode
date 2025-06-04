9c569fc50fc03a5d624ee1586913c04ff2027670
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-06-04 19:34:58 +0100

    added new solution

---
+++ go/17.LetterCombinationsofaPhoneNumber/solution.go
@@ -0,0 +1,26 @@
+package main
+
+func letterCombinations(digits string) []string {
+	m := make(map[byte][]string)
+	var sol []string
+
+	m['2'] = []string{"a", "b", "c"}
+	m['3'] = []string{"d", "e", "f"}
+	m['4'] = []string{"g", "h", "i"}
+	m['5'] = []string{"j", "k", "l"}
+	m['6'] = []string{"m", "n", "o"}
+	m['7'] = []string{"p", "q", "r", "s"}
+	m['8'] = []string{"t", "u", "v"}
+	m['9'] = []string{"w", "x", "y", "z"}
+	i := 0
+	for j, _ := range digits {
+		if val, ok := m[digits[i]]; !ok {
+			continue
+		}
+		if val, ok := m[digits[j]]; !ok {
+			continue
+		}
+
+	}
+
+}
